d2bb7ffdc6528f331554dbf09abec4e2a477bb13
ED <edwincarounity@gmail.com>
2025-12-01 17:03:10 -0500

    creacion script playerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //variables del movimiento del personaje
+    public float speed = 5;
+
+    //variables del salto del personaje
+    public float jumpForce = 6f;
+
+    private Rigidbody2D playerRigidbody;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Jump()
+    {
+
+    }
+}
